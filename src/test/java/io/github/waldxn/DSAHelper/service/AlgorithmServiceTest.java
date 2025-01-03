315643dec9ad5b8209f1d2305282241793f7b134
Waldxn <29243129+Waldxn@users.noreply.github.com>
2025-01-02 21:33:00 -0500

    Implemented unit test

--- src/test/java/io/github/waldxn/DSAHelper/service/AlgorithmServiceTest.java
+++ src/test/java/io/github/waldxn/DSAHelper/service/AlgorithmServiceTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.test.util.AssertionErrors.assertEquals;
@@ -17,18 +18,30 @@ public class AlgorithmServiceTest {
 
     @Test
     public void testBubbleSortSortedInput() {
-        List<Integer> input = List.of(1, 2, 3, 4, 5);
-        List<List<Integer>> expectedSteps = List.of(input);
+        var input = List.of(1, 2, 3, 4, 5);
+        var expectedSteps = List.of(input);
 
-        List<List<Integer>> resultSteps = algorithmService.bubbleSort(input);
-
-        assertEquals("Sorted input steps do not match", expectedSteps, resultSteps);
+        assertEquals("Sorted input steps do not match", expectedSteps, algorithmService.bubbleSort(input));
     }
 
     @Test
     public void testBubbleSortReversedInput() {
-        List<Integer> input = List.of(1, 2, 3, 4, 5);
-        List<List<Integer>> expectedSteps;
+        var input = new ArrayList<>(List.of(5, 4, 3, 2, 1));
+        var expectedSteps = List.of(
+                List.of(5, 4, 3, 2, 1),
+                List.of(4, 5, 3, 2, 1),
+                List.of(4, 3, 5, 2, 1),
+                List.of(4, 3, 2, 5, 1),
+                List.of(4, 3, 2, 1, 5),
+                List.of(3, 4, 2, 1, 5),
+                List.of(3, 2, 4, 1, 5),
+                List.of(3, 2, 1, 4, 5),
+                List.of(2, 3, 1, 4, 5),
+                List.of(2, 1, 3, 4, 5),
+                List.of(1, 2, 3, 4, 5)
+        );
+
+        assertEquals("Reversed input does not match", expectedSteps, algorithmService.bubbleSort(input));
 
     }
 }
