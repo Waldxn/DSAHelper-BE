d520a0fbfe4b497593c1ef4238e361771ad29f61
Waldxn <29243129+Waldxn@users.noreply.github.com>
2025-01-02 21:32:43 -0500

    CORS allows Postman

--- src/main/java/io/github/waldxn/DSAHelper/config/CorsConfig.java
+++ src/main/java/io/github/waldxn/DSAHelper/config/CorsConfig.java
@@ -7,11 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    // TODO: Fix this to work with Postman
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all endpoints
-                .allowedOrigins("http://localhost") // Frontend origin
+                .allowedOrigins("http://localhost", "http://localhost:4200") // Frontend origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow cookies/auth headers
